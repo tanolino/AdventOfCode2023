f9f7995a60c77bee4ac6b263b45d36fd1c533fc4
Nathanael Krauße <taniga_danae@gmx.de>
2023-12-03 13:10:44 +0100

    [ADD] Day 1 part 2 solution

--- day_01/code.exs
+++ day_01/code.exs
@@ -16,25 +16,67 @@ defmodule Processing do
     |> Enum.filter(&Processing.is_number/1)
   end
 
-  def line_value(line) do
+
+  @nums %{"one" => "1", "two" => "2", "three" => "3", "four" => "4", "five" => "5", "six" => "6", "seven" => "7", "eight" => "8", "nine" => "9"}
+
+  def string_to_numbers_from_left(line) do
+    index = Enum.min_by(@nums, fn x -> 
+      String.split(line, elem(x, 0)) \
+      |> List.first() \
+      |> String.length()
+    end)
+    String.replace(line, elem(index, 0), elem(index, 1), global: false)
+  end
+
+  def string_to_numbers_from_right(line) do
+    index = Enum.min_by(@nums, fn x -> 
+      String.split(line, elem(x, 0)) \
+      |> List.last() \
+      |> String.length()
+    end)
+    String.replace(line, elem(index, 0), elem(index, 1))
+
+  end
+
+  def line_value_part1(line) do
     nums = numbers(line)
-    left = List.first(nums, default: "0")
-    right = List.last(nums, default: "0")
+    left = List.first(nums) || "0"
+    right = List.last(nums) || "0"
+    
+    #IO.puts(left <> " " <> right)
     String.to_integer(left <> right)
   end
 
-  def file_value(lines) do
-    Enum.map(lines, &Processing.line_value/1) \
+  def line_value_part2(line) do
+    left = Processing.string_to_numbers_from_left(line) \
+           |> numbers() \
+           |> List.first() || "0"
+    right = Processing.string_to_numbers_from_right(line) \
+            |> numbers() \
+            |> List.last() || "0"
+    
+    #IO.puts(left <> " " <> right)
+    String.to_integer(left <> right)
+  end
+
+  def file_value(lines, func) do
+    Enum.map(lines, func) \
     |> List.foldl(0, fn (x, acc) -> x + acc end)
   end
 
-  def for_each_file(files) do
+  def for_each_file(files, func) do
     Enum.map(files, fn (file) ->
       value = Input.get(file) \
-              |> Processing.file_value()
+              |> Processing.file_value(func)
       IO.puts(file <> " : " <> to_string(value)) 
     end)
   end
 end
 
-Processing.for_each_file(["example_1.txt", "input_1.txt"])
+files = ["example_1.txt", "example_2.txt", "input_1.txt"]
+
+IO.puts("Day 1")
+Processing.for_each_file(files, &Processing.line_value_part1/1)
+IO.puts("")
+IO.puts("Day 2")
+Processing.for_each_file(files, &Processing.line_value_part2/1)
