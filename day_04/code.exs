bed230ed9149a59e97cc3a9390af472d716a7bcb
Nathanael Krauße <taniga_danae@gmx.de>
2023-12-05 20:14:38 +0100

    [ADD] Day 4 Part 2

--- day_04/code.exs
+++ day_04/code.exs
@@ -39,10 +39,39 @@ defmodule Part1 do
   end
 end
 
+defmodule Part2 do
+  def value_one_card(card) do
+    win = elem(card, 1)
+    my = elem(card, 2)
+    length(win -- (win -- my))
+  end
+
+  def count_cards([], _doubles_list, acc) do acc end
+  def count_cards([first| tail], doubles_list, acc) do
+    draw_amount = (1 + length(doubles_list))
+    score = Part2.value_one_card(first)
+    new_doubles_list = doubles_list
+                       |> Enum.map(fn x -> x - 1 end)
+                       |> Enum.filter(fn x -> x > 0 end)
+    duplicates = if score > 0 do 
+      List.duplicate(score, draw_amount)
+    else
+      []
+    end
+    count_cards(tail, new_doubles_list ++ duplicates, acc + draw_amount)
+  end
+end
 
 files = ["example_1.txt", "input_1.txt"]
+IO.puts("Part 1:")  
 Enum.map(files, fn file ->
   score = In.get_cards(file)
           |>Part1.value_cards()
+  IO.puts(file <> " score: " <> to_string(score))
+end)
+IO.puts("\nPart 2:")
+Enum.map(files, fn file ->
+  score = In.get_cards(file)
+          |>Part2.count_cards([], 0)
   IO.puts(file <> " score: " <> to_string(score))
 end)
